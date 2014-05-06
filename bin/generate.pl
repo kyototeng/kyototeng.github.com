62fd650ba08ac8d24abd5ecca2d96bc0ec78fdb6
Eric Johnson <eric.git@iijo.org>
2014-05-05 21:24:02 -0500

    Initial commit

---
+++ bin/generate.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use lib 'local/lib/perl5';
+
+use Text::Xslate;
+use Path::Class;
+use YAML qw/LoadFile/;
+
+foreach my $mode (qw/emacs vi/) {
+    my $in_dir   = dir('root');
+    my $out_dir  = dir('public');
+    my $out_file = $out_dir->file("$mode.html");
+
+    my $columns  = LoadFile "root/data-$mode.yml";
+    my $vars     = { columns => $columns };
+    my $string   = Text::Xslate->new(path => $in_dir)
+        ->render("$mode.html", $vars);
+
+    $out_dir->mkpath;
+    $out_file->spew($string);
+}
+
